a667f45c8c5cbc48733559a787c6a3d2637738c3
Alex_Hripko <2000alexh@gmail.com>
2024-08-03 17:23:37 +0300

    Update

--- src/main/java/files/lesson_4_4_9/RealMailService.java
+++ src/main/java/files/lesson_4_4_9/RealMailService.java
@@ -3,7 +3,6 @@ package files.lesson_4_4_9;
 public class RealMailService implements MailService {
     @Override
     public Sendable processMail(Sendable mail) {
-        // Здесь описан код настоящей системы отправки почты.
         return mail;
     }
 }
