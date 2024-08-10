2cb1bd761dc4b2f55a48b797972dbd2f2680e607
Alex_Hripko <2000alexh@gmail.com>
2024-08-10 14:03:38 +0300

    Задача 5.2.15

---
+++ src/main/java/generics_collections/lesson_5_2_15/Main.java
@@ -0,0 +1,23 @@
+package generics_collections.lesson_5_2_15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        try (BufferedReader str = new BufferedReader(new InputStreamReader(System.in))) {
+            String[] line = str.readLine().split(" ");
+            for (int i = 0; i < line.length; i++) {
+                if ((i & 1) != 0) {
+                    list.addFirst(line[i]);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        System.out.println(list);
+    }
+}
