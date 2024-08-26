5cc9230944f3d89d58c8214f4c479a3ed96ac9d6
Alex_Hripko <2000alexh@gmail.com>
2024-08-26 22:48:08 +0300

    Задание 6.1.9

---
+++ src/main/java/function_interface/lesson_6_1_9/Main.java
@@ -0,0 +1,8 @@
+﻿package function_interface.lesson_6_1_9;
+
+public class Main {
+    public static void main(String[] args) {
+        NumberGenerator<Number> generator = (NumberGenerator<Number>) NumberGenerator.getGenerator();
+        System.out.println(generator.cond(-4));
+    }
+}
---
+++ src/main/java/function_interface/lesson_6_1_9/NumberGenerator.java
@@ -0,0 +1,10 @@
+﻿package function_interface.lesson_6_1_9;
+
+@FunctionalInterface
+public interface NumberGenerator<T extends Number> {
+    boolean cond(T arg);
+
+    static NumberGenerator<? super Number> getGenerator() {
+        return (Number x) -> x.intValue() > 0;
+    }
+}
